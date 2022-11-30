c90a72f3e27d0a1710cef2bf4160fb0facea8a4b
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-11-29 20:53:51 -0800

    Added tailwindcss styling in h1 classname

--- issa-mme_frontend/src/App.js
+++ issa-mme_frontend/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const App = () => {
-  return <div>Viva Mexico!</div>;
+  return <h1 className="text-3xl font-bold underline">Viva Mexico!</h1>;
 };
 
 export default App;
