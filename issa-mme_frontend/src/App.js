78f4e3f94553459f3a1cbe987e36385d0a524721
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-11-29 21:51:29 -0800

    Import Routes, Route, useNavigate. +Login +Home

--- issa-mme_frontend/src/App.js
+++ issa-mme_frontend/src/App.js
@@ -1,7 +1,13 @@
 import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
 
 const App = () => {
-  return <h1 className="text-3xl font-bold underline">Viva Mexico!</h1>;
+  return (
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
+  );
 };
 
 export default App;
