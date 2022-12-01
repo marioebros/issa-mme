2bd68bbb2b3babc74825a5e6b8db5dfb75f4b30f
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-11-30 16:45:37 -0800

    Import Sidebar + UserProfile to Home.jsx

--- issa-mme_frontend/src/container/Home.jsx
+++ issa-mme_frontend/src/container/Home.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useRef, useEffect } from "react";
-import { HiMenu } from "react-icons/hi";ai
+import { HiMenu } from "react-icons/hi";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { Link, Route, Routes } from "react-router-dom";
+
+import Sidebar from "../components/Sidebar";
+import UserProfile from "../components/UserProfile";
 
 const Home = () => {
   return (
