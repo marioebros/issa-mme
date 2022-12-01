1903a61ba3aac920a1104c717429404067a2e387
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-11-30 16:58:26 -0800

    Import Pins, client, logo, and index.js

--- issa-mme_frontend/src/container/Home.jsx
+++ issa-mme_frontend/src/container/Home.jsx
@@ -3,8 +3,10 @@ import { HiMenu } from "react-icons/hi";
 import { AiFillCloseCircle } from "react-icons/ai";
 import { Link, Route, Routes } from "react-router-dom";
 
-import Sidebar from "../components/Sidebar";
-import UserProfile from "../components/UserProfile";
+import { Sidebar, UserProfile } from "../components";
+import Pins from "./Pins";
+import { client } from "../client";
+import logo from "../assets/logo.png";
 
 const Home = () => {
   return (
