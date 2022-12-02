5bc0d096fb6c5aca6e0ff024c76a11908898719d
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-02 15:52:42 -0800

    Added onClick function + const categories

--- issa-mme_frontend/src/components/Sidebar.jsx
+++ issa-mme_frontend/src/components/Sidebar.jsx
@@ -8,6 +8,15 @@ import logo from "../assets/logo.png";
 const isNotActiveStyle = "flex items-center px-5 gap-3 text-gray-500 hover:text-black transition-all duration-200 ease-in-out capitalize";
 const isActiveStyle = "flex items-center px-5 gap-3 font-extrabold border-r-2 border-black transition-all duration-200 ease-in-out capitalize";
 
+const categories = [
+  { name: "Animals"},
+  { name: "Wallpapers"},
+  { name: "Photography"},
+  { name: "Food"},
+  { name: "Coding"},
+  { name: "Other"},
+]
+
 const Sidebar = ({ user, closeToggle }) => {
   const handleCloseSidebar = () => {
     if(closeToggle) closeToggle(false);
@@ -27,10 +36,22 @@ const Sidebar = ({ user, closeToggle }) => {
           <NavLink
             to="/"
             className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+            onClick={handleCloseSidebar}
           >
             <RiHomeFill />
             Home
           </NavLink>
+          <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover Categories</h3>
+          {categories.slice(0, categories.length - 1).map((category) => (
+            <NavLink
+              to={`/category/${category.name}`}
+              className={({ isActive }) => isActive ? isActiveStyle : isNotActiveStyle}
+              onClick={handleCloseSidebar}
+              key={category.name}
+            >
+              {category.name}
+            </NavLink>
+          ))}
         </div>
       </div>
     </div>
