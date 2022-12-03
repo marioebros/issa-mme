fdd2b170d06cc3c8c55d226be9e7cf4fdae41fcc
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-02 16:30:17 -0800

    Added styling to Sidebar

--- issa-mme_frontend/src/components/Sidebar.jsx
+++ issa-mme_frontend/src/components/Sidebar.jsx
@@ -54,6 +54,15 @@ const Sidebar = ({ user, closeToggle }) => {
           ))}
         </div>
       </div>
+      {user && (
+        <Link
+          to={`user-profile/${user._id}`}
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg"
+        >
+          <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+          <p>{user.userName}</p>
+        </Link>
+      )}
     </div>
   )
 }
