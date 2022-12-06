cdeeec249cedb06b701852f2ae796ff15efa2c52
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-05 21:59:15 -0800

    Add mx-3 to className line 60 + onClick to line 61

--- issa-mme_frontend/src/components/Sidebar.jsx
+++ issa-mme_frontend/src/components/Sidebar.jsx
@@ -57,7 +57,8 @@ const Sidebar = ({ user, closeToggle }) => {
       {user && (
         <Link
           to={`user-profile/${user._id}`}
-          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg"
+          className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+          onClick={handleCloseSidebar}
         >
           <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
           <p>{user.userName}</p>
