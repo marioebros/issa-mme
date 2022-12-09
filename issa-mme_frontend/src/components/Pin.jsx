b39567a85c5cd63e7a6c42a8cf2d4b3f63e02972
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-08 21:54:26 -0800

    Import urlFor + img element for user-posts

--- issa-mme_frontend/src/components/Pin.jsx
+++ issa-mme_frontend/src/components/Pin.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { urlFor } from '../client'
 
-const Pin = () => {
+const Pin = ({ pin: { postedBy, image, _id, destination }}) => {
   return (
-    <div>Pin</div>
+    <div>
+        <img className="rounded-lg w-full" alt="user-post" src={urlFor(image).width(250).url()} />
+    </div>
   )
 }
 
