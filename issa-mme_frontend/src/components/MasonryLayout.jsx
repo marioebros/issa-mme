d602a124d841a38e6f23759b11a94d598ea87950
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-08 20:37:10 -0800

    Added const breakpointObj layout + Masonry element

--- issa-mme_frontend/src/components/MasonryLayout.jsx
+++ issa-mme_frontend/src/components/MasonryLayout.jsx
@@ -3,9 +3,20 @@ import Masonry from "react-masonry-css";
 
 import Pin from "./Pin";
 
+const breakpointObj = {
+    default: 4,
+    3000: 6,
+    2000: 5,
+    1200: 3,
+    1000: 2,
+    500: 1,
+}
+
 const MasonryLayout = () => {
   return (
-    <div>MasonryLayout</div>
+    <Masonry className="flex animate-slide-fwd">
+
+    </Masonry>
   )
 }
 
