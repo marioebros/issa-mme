a3d80e1c689e9a5995bea7780e238a359e80e12e
Mario M. Elizalde <dollarize.mme@gmail.com>
2022-12-08 16:02:12 -0800

    Added loading/set const + if condition for Spinner

--- issa-mme_frontend/src/components/Feed.jsx
+++ issa-mme_frontend/src/components/Feed.jsx
@@ -6,6 +6,9 @@ import MasonryLayout from "./MasonryLayout";
 import Spinner from "./Spinner";
 
 const Feed = () => {
+  const [loading, setloading] = useState(true);
+
+  if(loading) return <Spinner message="We are adding new ideas to your feed!" />
   return (
     <div>Feed</div>
   )
